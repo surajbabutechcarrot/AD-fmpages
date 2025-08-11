facb10a263957f7f96232407cf2f6633aa5864f7
surajbabutechcarrot <101328482+surajbabutechcarrot@users.noreply.github.com>
2025-08-11 15:14:14 +0400

    latest

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <!-- Lazyload -->
+    <script src="assets/js/vendors/lazyload-all.js" async=""></script>
+    <!-- <script src="assets/js/vendors/slick.min.js"></script> -->
+    
+    <!-- tweenmax animation -->
+    <!-- <script type="text/javascript" src="assets/js/TweenMax.min.js"></script>
+    <script type="text/javascript" src="assets/js/ScrollMagic.js"></script>
+    <script type="text/javascript" src="assets/js/animation.gsap.min.js"></script>
+    <script type="text/javascript" src="assets/js/animation.js"></script> -->
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script>
+        function loadJS(u) {
+        var r = document.getElementsByTagName("script")[0],
+            s = document.createElement("script");
+            s.src = u;
+            r.parentNode.insertBefore(s, r);
+        }
+    </script>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/main.min.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <header class="main-header">
+        <div class="container h-100 d-flx flx-vcenter">
+            <div class="logo">
+                <a href="">
+
+                </a>
+            </div>
+            <div class="header-right ml-auto d-flx flx-vcenter">
+                <nav class="main-nav">
+                    <ul>
+                        <li><a class="active" href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Services</a>
+                            <ul>
+                                <li><a href="">Services 1</a>
+                                    <ul>
+                                        <li><a href="">Services 1</a></li>
+                                        <li><a href="">Services 2</a></li>
+                                        <li><a href="">Services 3</a></li>
+                                        <li><a href="">Services 4</a></li>
+                                        <li><a href="">Services 5</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="">Services 2</a></li>
+                                <li><a href="">Services 3</a></li>
+                                <li><a href="">Services 4</a></li>
+                                <li><a href="">Services 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Get an Alert</a>
+                            <ul>
+                                <li><a href="">Services 1</a></li>
+                                <li><a href="">Services 2</a></li>
+                                <li><a href="">Services 3</a></li>
+                                <li><a href="">Services 4</a></li>
+                                <li><a href="">Services 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Sign in/Sign up</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="mob-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="overlay"></div>
+    </header>
